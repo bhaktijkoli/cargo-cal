fb280cb7dbeb280ac9c250bfd8e176df892d425f
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-22 12:46:28 +0530

    Added navbar

--- client/js/app/App.jsx
+++ client/js/app/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { connect } from "react-redux";
 
+import Navbar from './Layout/Navbar.jsx';
 import Home from './Home/Home.jsx';
 
 class App extends Component {
   render() {
     return (
       <div id="wrapper">
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home}/>
         </Switch>
